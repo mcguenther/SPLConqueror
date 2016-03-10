a2b9dcc6fa15dc79fadd75505e722170d6c2dcae
AlexanderGrebhahn <alexandergrebhahn@gmail.com>
2016-03-10 09:33:41 +0100

    added missing classes

---
+++ SPLConqueror/Dune/TemplateObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dune
+{
+    class TemplateObject
+    {
+
+        public enum Kind {concrete, placeholder};
+
+        public Kind type;
+        public Boolean isTerminal = true;
+
+        public DuneFeature referseTo = null;
+
+        public string artificalString = "";
+
+        public Dictionary<String, String> referseToAliasing = new Dictionary<string, string>();
+
+        private List<TemplateObject> children;
+
+        private DuneClass defaultValue = null;
+
+
+
+    }
+}
---
+++ SPLConqueror/Dune/TemplateTree.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dune
+{
+    class TemplateTree : TemplateObject
+    {
+        List<TemplateTree> children = new List<TemplateTree>();
+
+        TemplateTree currElement;
+        TemplateTree parent;
+
+        String furtherInformation = "";
+
+        public TemplateTree()
+        {
+            currElement = this;
+            
+        }
+
+
+        internal TemplateTree lastChild()
+        {
+            return currElement.children[currElement.children.Count - 1];
+        }
+
+        internal void incHierarchy()
+        {
+            TemplateTree nonTerminal = lastChild();
+            nonTerminal.isTerminal = false;
+            currElement = nonTerminal;
+        }
+
+        internal void decHierarchy()
+        {
+            currElement = currElement.parent;
+        }
+
+        internal void addFurtherInformation(string token)
+        {
+            this.furtherInformation = token;
+        }
+
+        internal void addInformation(string token)
+        {
+            TemplateTree newPart = new TemplateTree();
+
+            if (XMLParser.nameWithoutPackageToDuneFeatures.ContainsKey(token))
+            {
+
+                XMLParser.easyToFind += 1;
+                if (XMLParser.nameWithoutPackageToDuneFeatures[token].Count > 1)
+                {
+                    XMLParser.mehrdeutigkeit += 1;
+                    Console.WriteLine("TODO:: addInformation with mehrdeutigkeit");
+                }
+                
+                newPart.referseTo = XMLParser.nameWithoutPackageToDuneFeatures[token].First();
+                newPart.artificalString = token;
+                newPart.type = Kind.concrete;
+                newPart.isTerminal = true;
+            }
+            else
+            {
+                newPart.artificalString = token;
+                newPart.type = Kind.placeholder;
+                newPart.isTerminal = true;
+            }
+
+            newPart.parent = currElement;
+            currElement.children.Add(newPart);
+        }
+
+        public String toString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (this.referseTo != null)
+                sb.Append(this.referseTo.ToString());
+            else
+                sb.Append(artificalString);
+
+            if(this.children.Count > 0)
+                sb.Append(" ( ");
+
+            for (int i = 0; i < this.children.Count; i++)
+            {
+                sb.Append(this.children[i].toString()+ " ");
+            }
+
+            if (this.children.Count > 0)
+                sb.Append(" ) ");
+
+            sb.Append(furtherInformation);
+            return sb.ToString();
+
+        }
+
+
+        /// <summary>
+        /// This methods return a preorder flattering of the template tree. 
+        /// </summary>
+        /// <returns></returns>
+        public List<TemplateTree> flatten()
+        {
+            List<TemplateTree> elements = new List<TemplateTree>();
+
+            elements.Add(this);
+            for (int i = 0; i < this.children.Count; i++)
+            {
+                elements.AddRange(this.children[i].flatten());
+            }
+            return elements;
+        }
+
+    }
+}
